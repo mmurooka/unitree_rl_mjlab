71b0950d710cb865834c1ab4392a8504690d017d
Masaki Murooka <m-murooka@aist.go.jp>
2026-09-18 16:22:49 +0900

    Pin MuJoCo and Warp versions and add SciPy dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 """Installation script for the 'unitree_rl_mjlab' python package."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # Minimum dependencies required prior to installation
 INSTALL_REQUIRES = [
     "mjlab==1.2.0",
     "pyzmq",
+    "mujoco==3.5.0",
     "mujoco-warp==3.5.0",
+    "warp-lang==1.12.0",
+    "scipy",
 ]
 
 # Installation operation
